53c7acdc2eeb3264f64d9099d17a38c58b192b3c
Bereket Yimer <bekym0242@gmail.com>
2024-08-18 23:40:08 +0300

    create route

--- my-company/src/App.js
+++ my-company/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom'; 
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './component/Home'
+import About from './component/About'
+import Contact from './component/Contact'
+import Services from './component/Services'
+import Navbar from './component/Navbar'
 function App() {
   return (
-    <BrowserRouter>
-     <Routes></Routes>
-    </BrowserRouter>
+    <Routes path="/" element={<Navbar />} >
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="services" element={<Services />} />
+    </Routes>
   );
 }
 
