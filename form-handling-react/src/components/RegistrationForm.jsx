3b9082b7ae0d2d9e3a01297357e8ad969eadceef
Bereket Yimer <bekym0242@gmail.com>
2024-09-01 12:10:48 +0300

    controlled-form

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export default function RegistrationForm() {
+    const [formData, setFormData] = useState({ userName: '', email: '', password: '' });
+    const handelChange = (e) => {
+        const [name, value] = e.target;
+        setFormData({ ...formData, [name]: value })
+
+    }
+    const handelSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+    return (
+        <>
+            <h1>Registration Form</h1>
+            <form onSubmit={handelSubmit}>
+                <input type='text' name='userName' value={formData.userName} onChange={handelChange}>User Name</input>
+                <input type='email' name='email' value={formData.email} onChange={handelChange}>Email</input>
+                <input type='password' name='password' value={formData.password} onChange={handelChange}>Password</input>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
+}
