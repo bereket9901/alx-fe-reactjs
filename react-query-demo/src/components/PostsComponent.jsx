c0a07cc37ae26201635a27427b8f9769cd61f4c3
Bereket Yimer <bekym0242@gmail.com>
2024-09-01 14:28:56 +0300

    asd

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,10 +2,13 @@ import { useQuery } from "react-query"
 
 const fetchPosts = async () => {
     const result = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if (!result.ok) {
+        throw new Error('Network response was not ok');
+    }
     return result.json();
 }
 export default function PostsComponent() {
-    const { data, isError ,error, isLoading } = useQuery('fetchData', fetchPosts);
+    const { data, isError, isLoading } = useQuery('fetchData', fetchPosts);
     if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error: {isError.message}</div>;
     return (
