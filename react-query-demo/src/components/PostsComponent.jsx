4bbaaeaf82a9c2fa3174b0522552635a3faaa982
Bereket Yimer <bekym0242@gmail.com>
2024-09-01 14:22:52 +0300

    use useQuery

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,17 @@
+import { useQuery } from "react-query"
+
+const fetchData = async () => {
+    const result = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return result.json();
+}
+export default function PostsComponent() {
+    const { data, error, isLoading } = useQuery('fetchData', fetchData);
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+    return (
+        <>
+            <h1>Posts Component</h1>
+            {data.map(item => (<div key={item.id}>{item.title}</div>))}
+        </>
+    )
+}
