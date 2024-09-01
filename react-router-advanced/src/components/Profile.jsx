8e9d3122760325bf35c252679a45cf0565555458
Bereket Yimer <bekym0242@gmail.com>
2024-09-01 21:11:27 +0300

    profile routes

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,10 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useMatch } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 function Profile() {
+    const match = useMatch('/profile/*');
+    const url = match ? match.pathnameBase : '';
     return (
         <>
             <h1>Profile</h1>
-            <Outlet />
+            <Router>
+                <Routes>
+                    <Route Route path={`${url}/profile`} element={<Profile />}>
+                        <Route path="profile-details" element={<ProfileDetails />} />
+                        <Route path="profile-settings" element={<ProfileSettings />} />
+                    </Route>
+                </Routes>
+            </Router>
         </>
     )
 }
