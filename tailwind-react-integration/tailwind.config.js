c4cf25cf73be6374dc96b531da55040d6e1b1747
Bereket Yimer <bekym0242@gmail.com>
2024-09-07 18:04:36 +0300

    tailwind installation

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  variants: { extend: {} },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+}
+
