1d63b2d5b57c8a4de2602253124ded67ac51374c
Bereket Yimer <bekym0242@gmail.com>
2024-09-15 07:33:18 +0300

    fix link

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import data from '../data.json';
+import { Link } from 'react-router-dom';
 
 
 export default function HomePage() {
@@ -10,17 +11,19 @@ export default function HomePage() {
 
     return (<>
         <h1>Recipe</h1>
-        <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
-            {recipeData && recipeData.map((recipe, index) => {
-                return (
-                    <div className='bg-slate-800 px-1 py-5 m-10 w-72 shadow-blue-900 shadow-lg rounded-3xl hover:shadow-2xl hover:shadow-blue-500' key={index}>
-                        <img src={recipe.image} className='mx-auto m-5' alt="recipe-image" />
-                        <h2 className='font-bold text-lg'>{recipe.title}</h2>
-                        <p className='text-sm'>{recipe.summary}</p>
-                    </div>
-                );
-            })}
-        </div>
+        <Link to="./recipeDetails">
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
+                {recipeData && recipeData.map((recipe, index) => {
+                    return (
+                        <div className='bg-slate-800 px-1 py-5 m-10 w-72 shadow-blue-900 shadow-lg rounded-3xl hover:shadow-2xl hover:shadow-blue-500' key={index}>
+                            <img src={recipe.image} className='mx-auto m-5' alt="recipe-image" />
+                            <h2 className='font-bold text-lg'>{recipe.title}</h2>
+                            <p className='text-sm'>{recipe.summary}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </Link>
     </>
 
     );
