5c90a67dbb9007ad7f245bcb3837c9838a11cce5
Bereket Yimer <bekym0242@gmail.com>
2024-09-22 22:13:33 +0300

    services

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,18 @@
-import axios from 'axios';
+export const fetchAdvancedSearchData = async ({ username, location, minRepos }, page = 1) => {
+  const query = [];
+  if (username) query.push(`user:${username}`);
+  if (location) query.push(`location:${location}`);
+  if (minRepos) query.push(`repos:>=${minRepos}`);
 
-const GITHUB_API_URL = 'https://api.github.com/users';
+  const searchQuery = query.join('+');
 
-export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`${GITHUB_API_URL}/${username}`);
-    return response.data;
+    const response = await axios.get(`${GITHUB_SEARCH_API_URL}?q=${searchQuery}&page=${page}`);
+    return {
+      data: response.data,
+      headers: response.headers,
+    };
   } catch (error) {
-    throw new Error('Failed to fetch user data');
+    throw new Error('Failed to fetch search data');
   }
 };
